e7f487f6b17eb1e2c5bd827d013ecd4cd2dfecc2
우주호 <einjuho7@ajou.ac.kr>
2020-01-30 18:04:29 +0900

    added 0130

--- 00_algorithm/algo_basic/src/day4/FiboTest.java
+++ 00_algorithm/algo_basic/src/day4/FiboTest.java
@@ -1,19 +1,28 @@
 package day4;
 
+import java.util.Arrays;
+
 public class FiboTest {
 
+	
+	static long [] memo = new long[100];
+	
 	public static void main(String[] args) {
 		// fibo(10)을 구해보자
-		System.out.println(fibo(10));
+		memo[0] = 0;
+		memo[1] = 1;
+		System.out.println(fibo(50));
+		System.out.println(Arrays.toString(memo));
+		
 	}
 	
-	private static int fibo(int num) {
-		if(num < 2) {
-			return 1;
-		}else
-		{
-			return num+ fibo(num-1);
-		}	
+	private static long fibo(int num) {
+		if(memo[num] ==0 && num>1) {
+			memo[num] = fibo(num-1) + fibo(num-2);
+		}
+		
+		return memo[num];
+		
 	}
 
 }

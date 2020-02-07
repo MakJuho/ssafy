12a7caa7bb3b90b8b0137440400e951123da9956
우주호 <einjuho7@ajou.ac.kr>
2020-02-07 18:02:24 +0900

    add 오셀로

--- 00_algorithm/algo_basic/src/SWEA/SWEA_4615_오셀로게임.java
+++ 00_algorithm/algo_basic/src/SWEA/SWEA_4615_오셀로게임.java
@@ -40,16 +40,33 @@ public class SWEA_4615_오셀로게임 {
 					
 			for(int i=0; i<cnt; i++) {
 				
-				int c = sc.nextInt()-1;
-				int r = sc.nextInt()-1;
-				
+				Pair pos = null;
+				pos.y = sc.nextInt()-1;
+				pos.x = sc.nextInt()-1;
+						
 				int color = sc.nextInt(); // 1: 검정 , 2: 흰색
 
-				// 방향 별로 탐색
+				for(int j=0; j<4; j++) {
 //				1. 한칸만 이동해서 경계선 초과했는지 ? 0인지? 아니면 반대 색 돌을 가지고 있는지
+
+					if(pos.x+dx[j]>=0 && pos.y+dy[j]>=0 && pos.x+dx[j] <N && pos.y+dy[j] < N) {
+						
+						// 주변 살폈을 때 0인가?
+						if(board[pos.x+dx[j]][pos.y+dy[j]] == 0) {
+							
+						}else if(board[pos.x+dx[j]][pos.y+dy[j]] != color) {
+						// 주변 바둑돌 색이 반대 색을 가지고 있는가?
+						// 뭔가를 코딩	
+						}
+						
+						
+					}
+				}
+				
+				// 방향 별로 탐색
 //				2. 같은 색 나올 때까지 탐색
 //				3. 같은 색 나오면 좌표를 기억해놨다가 시작 점부터 그 사이 좌표 색깔 모두 변경. for문 두번
-				
+			
 				
 				
 				

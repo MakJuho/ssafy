be52e66f0bfa9183851cfcdd1fb759c422873299
우주호 <einjuho7@ajou.ac.kr>
2020-02-10 18:02:58 +0900

    added 정올 냉장고

--- 00_algorithm/algo_basic/src/JungOl/JA_1828_냉장고.java
+++ 00_algorithm/algo_basic/src/JungOl/JA_1828_냉장고.java
@@ -2,14 +2,31 @@ package JungOl;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class JA_1828_냉장고 {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
+		
 		System.setIn(new FileInputStream("res\\1828.txt"));
 		Scanner sc = new Scanner(System.in);
+		
+		int TC = sc.nextInt();
+		int[][] arr = new int[TC+1][2];
+		int[] temp = new int[10271];
+		
+		for(int t=1; t<=TC; t++) {
+			arr[t][0] = sc.nextInt()+270; // low
+			arr[t][1] = sc.nextInt()+270; // high
+			
+			for(int i=arr[t][0]; i<arr[t][1]; i++) {
+				temp[i]++;
+			}
+			
+		}
+	
 		
 		
 	}
